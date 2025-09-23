c00724a47dd4e73d877f5e5a56f564e5efceaa4f
asma2003 <asma200m2200@gmail.com>
2025-09-23 13:43:04 +0300

    First update in Tuesday

--- bank/account.py
+++ bank/account.py
@@ -1,8 +1,26 @@
 class BankAccount:
-    def __init__(self, account_id, frst_name, last_name, password, balance_checking, balance_savings):
-        self.account_id = account_id
-        self.frst_name = frst_name
-        self.last_name = last_name
-        self.password = password
-        self.balance_checking = float(balance_checking)
-        self.balance_savings = float(balance_savings)
+    def __init__(self, account_id: str, balance: float = 0.0):
+        self.account_id = account_id 
+        self.balance = balance 
+        self.overdart_account = 0 
+        self.is_active = True 
+        self.overdart_limit = -100.0 
+        self.overdart = 35.0
+
+        def deposit(self, amount: float) -> bool:
+            """Debosit money into account"""
+
+            if amount <= 0
+
+            return False 
+             self.balance += amount 
+             return True 
+
+        def can_withdarw(self, amount: float) -> Tuple[bool, str]:
+            """Chack if withdrawl is allowed"""
+
+            if not self.is_active:
+                return False "Account is deactibated"
+            if amount <= 0:
+                return False, "Invailid withdrawl amount" 
+            new_balance = self.balance - amount           
