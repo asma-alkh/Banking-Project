4cfbbfe68736c86a3180d88244384a6dbeaaa8f4
asma2003 <asma200m2200@gmail.com>
2025-09-26 19:58:40 +0300

    Add BankCLI with menu and main run loop

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class BankCLI:
 
         try:
             account_id = int(input("Enter account ID: "))
-            password = input("Enter password")
+            password = input("Enter password: ")
             customer = self.system.authenticate(account_id, password)
             if customer:
                 # If authentication succeeds, store the logged-in customer
@@ -29,3 +29,41 @@ class BankCLI:
             # Handle case where account ID is not a number
             print("Invalid account ID.")
             return False
+
+    def show_menu(self):
+        #Display the available banking options to the user.
+        print("\n1. Deposit")
+        print("2. Withdraw")
+        print("3. Transfer")
+        print("4. check Balance")
+        print("5. logout")
+
+    def run(self):
+        # Main loop for the CLI, keeps running until user exist.
+        print("=== Welcome to My Bank ")    
+        while True:
+            if not self.current_customer:
+                # If no one is logged in, force login first
+                if not self.login():
+                    continue
+            # Show menu and get user choice
+            self.show_menu()
+            choice = input("select option: ")   
+
+            # Call corresponding function based on user choice
+
+            if choice == "1":
+                self.deposit_flow()
+            elif choice == "2":
+                self.withdraw_flow() 
+            elif choice == "3":
+                self.transfer_flow()
+            elif choice == "4":
+                self.check_balance()
+            elif choice == "5":
+                # Logout the current customer
+                self.current_customer = None
+                print("Logged out")
+            else:
+                print("Invalid choice.")    
+
